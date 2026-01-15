507f2290153f7bfb2cb2e018b4da9dd4722caafa
Pauputchi78 <pauetxp@gmail.com>
2026-01-15 23:23:34 +0100

    Creados Login, requierelogin y middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,32 @@
 // controllers/authController.js
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 async function login(req, res) {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
     if (!user) return res.status(401).json({ error: 'Credenciales inválidas' });
+    
+    const esCorrecta = await bcrypt.compare(password, user.password);
+    if (!esCorrecta) return res.status(401).json({ error: 'Credenciales inválidas' });
 
-    const ok = await user.comparePassword(password);
-    if (!ok) return res.status(401).json({ error: 'Credenciales inválidas' });
+    const token = jwt.sign(
+      { user_id: user._id, email: user.email },
+      process.env.JWT_SECRET, 
+      { expiresIn: '24h' }
+    );
 
-    req.session.user = { id: user._id, username: user.username, role: user.role };
-    res.json({ message: 'Login correcto', role: user.role });
+    res.json({
+      mensaje: '¡Bienvenido!',
+      token: token
+    });
+
   } catch (err) {
     res.status(500).json({ error: 'Error en login', detalle: err.message });
   }
 }
 
-function logout(req, res) {
-  req.session.destroy(() => {
-    res.json({ message: 'Sesión cerrada' });
-  });
-}
-
-module.exports = { login, logout };
+module.exports = { login } ;
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,23 @@
 // middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
 
 function requireLogin(req, res, next) {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'No autenticado' });
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if(!token) return res.status(401).json({ error: 'Acceso denegado. Debes iniciar sesión.' });
+
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+
+  }catch(error){
+    res.status(403).json({ error: 'Token inválido o expirado' });
   }
-  next();
+
 }
 
+//Falta implementar require role
 function requireRole(role) {
   return (req, res, next) => {
     if (!req.session.user || req.session.user.role !== role) {

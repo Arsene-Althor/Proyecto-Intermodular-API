ad9ed53b5e6af7a73be12649b1879208a3d2f7b6
Pauputchi_78 <pauetxp@gmail.com>
2026-02-16 14:31:36 +0100

    Cambio en validación modificación reserva

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -242,7 +242,7 @@ async function updateReservation(req, res) {
 
     let hoy = new Date();
 
-    if(hoy >= reservation.check_in) return res.status(400).json({ error: 'No es posible modificar la entrada de una reserva en curso' });
+    if(hoy >= reservation.check_in && reservation.check_in != nuevaEntrada) return res.status(400).json({ error: 'No es posible modificar la entrada de una reserva en curso' });
     if(nuevaEntrada >= nuevaSalida) return res.status(400).json({ error: 'La fecha de entrada no puede superar la fecha de salida' });
 
     if (reservation.check_in <= hoy || !check_in) {

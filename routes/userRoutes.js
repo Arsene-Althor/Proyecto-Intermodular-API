7fc303c10c797eddcfa1b827e93cfeb1e7980dd7
Arsen <almonteysael3@gmail.com>
2026-02-17 12:28:07 +0100

    Arreglo fallo subida imagen

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,32 @@
 //routes/authRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {registroUser, addEmployee, getAllUsers, removeUsers, modifyUser, updateDiscount} = require ('../controllers/userController.js');
-const {requireLogin, requireRole, requireOwnerOrAdmin} = require('../middleware/authMiddleware.js');
-const upload = require ('../middleware/diskStorage.js');
+const {
+  registroUser,
+  addEmployee,
+  getAllUsers,
+  removeUsers,
+  modifyUser,
+  updateDiscount,
+} = require("../controllers/userController.js");
+const {
+  requireLogin,
+  requireRole,
+  requireOwnerOrAdmin,
+} = require("../middleware/authMiddleware.js");
+const upload = require("../middleware/diskStorage.js");
 
 //Middleware para manejar errores de Multer
 const handleMulterError = (req, res, next) => {
     upload.single('profileImage')(req, res, (err) => {
-        if (err) {
-            console.error('Error de Multer:', err);
-            return res.status(400).json({ error: 'Error al subir imagen: ' + err.message });
-        }
-        next();
-    });
+    if (err) {
+      console.error("Error de Multer:", err);
+      return res
+        .status(400)
+        .json({ error: "Error al subir imagen: " + err.message });
+    }
+    next();
+  });
 };
 
 //Ruta publica, no necesita autenticar
@@ -21,14 +34,36 @@ const handleMulterError = (req, res, next) => {
 router.post('/register', handleMulterError, registroUser);
 
 //Protegidas (requiere que se logueen)
-router.post('/add', requireLogin, requireRole(['employee','admin','client']), upload.single('profileImage'), addEmployee); //POST /api/users/add (solo admin)
+router.post(
+  "/add",
+  requireLogin,
+  requireRole(["employee", "admin", "client"]),
+  handleMulterError,
+  addEmployee,
+); //POST /api/users/add (solo admin)
 
 router.get('/get', requireLogin, getAllUsers); //GET /api/users/get (solo usuarios logueados)
 
-router.patch('/modify/:userId', requireLogin, requireOwnerOrAdmin, upload.single('profileImage'), modifyUser); // PATCH /api/users/modif/:userId
+router.patch(
+  "/modify/:userId",
+  requireLogin,
+  requireOwnerOrAdmin,
+  handleMulterError,
+  modifyUser,
+); // PATCH /api/users/modif/:userId
 
-router.delete('/remove/:userId', requireLogin, requireRole(['admin', 'employee']), removeUsers); // DELETE /api/users/remove/:userId
+router.delete(
+  "/remove/:userId",
+  requireLogin,
+  requireRole(["admin", "employee"]),
+  removeUsers,
+); // DELETE /api/users/remove/:userId
 
-router.patch('/update/:userId', requireLogin, requireRole(['admin', 'employee']), updateDiscount); // PATCH /api/users/update/:userId
+router.patch(
+  "/update/:userId",
+  requireLogin,
+  requireRole(["admin", "employee"]),
+  updateDiscount,
+); // PATCH /api/users/update/:userId
 
 module.exports = router;
